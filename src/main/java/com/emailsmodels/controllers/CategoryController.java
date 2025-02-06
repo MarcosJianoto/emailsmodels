a2838f6f2aa1feeb5704e9cbf45ed5a068406f5c
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-02-05 21:18:07 -0300

    alterações

--- src/main/java/com/emailsmodels/controllers/CategoryController.java
+++ src/main/java/com/emailsmodels/controllers/CategoryController.java
@@ -2,6 +2,7 @@ package com.emailsmodels.controllers;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +19,7 @@ import com.emailsmodels.services.CategoryService;
 @RestController
 public class CategoryController {
 
+	@Autowired
 	private final CategoryService categoryService;
 
 	public CategoryController(CategoryService categoryService) {
